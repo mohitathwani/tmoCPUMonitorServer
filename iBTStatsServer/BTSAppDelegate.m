5690739064c53b8ab1b2df64d89a099091e08342
Mohit Athwani <mohit@geeksincorporated.net>
2013-05-11 03:41:27 +0530

    Initial Commit

---
+++ iBTStatsServer/BTSAppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  BTSAppDelegate.m
+//  iBTStatsServer
+//
+//  Created by Labs on 5/11/13.
+//  Copyright (c) 2013 TeraMoLabs. All rights reserved.
+//
+
+#import "BTSAppDelegate.h"
+
+@implementation BTSAppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ iBTStatsServer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iBTStatsServer
+//
+//  Created by Labs on 5/11/13.
+//  Copyright (c) 2013 TeraMoLabs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
